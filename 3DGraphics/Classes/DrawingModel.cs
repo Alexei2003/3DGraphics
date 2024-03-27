beeddc0dbc339d244f17dcc0b42e0da95a5d64d9
Aliaksei Dubouski <91179353+Alexei2003@users.noreply.github.com>
2024-03-27 03:37:05 +0300

    fis

--- 3DGraphics/Classes/DrawingModel.cs
+++ 3DGraphics/Classes/DrawingModel.cs
@@ -5,9 +5,9 @@ namespace _3DGraphics.Classes
 {
     internal static class DrawingModel
     {
-        public unsafe delegate void DrawObject(Point[] points, int[] vertexIndex, int* rgbBitmap, BitmapData bitmapData, int colorInt, int widthZone, int heightZone);
+        public unsafe delegate void DrawObject(PointF[] points, int[] vertexIndex, int* rgbBitmap, BitmapData bitmapData, int colorInt, int widthZone, int heightZone);
 
-        public static unsafe void Draw(Bitmap bitmap, GeometricVertex[] GeometricVertexСoordinates, int[][] GeometricVertexIndexs)
+        public static unsafe void Draw(Bitmap bitmap, GeometricVertex[] geometricVertexСoordinates, int[][] geometricVertexIndexs)
         {
             var widthZone = bitmap.Width - 1;
             var heightZone = bitmap.Height - 1;
@@ -28,7 +28,7 @@ namespace _3DGraphics.Classes
             var colorInt = Color.White.ToArgb();
 
             DrawObject drawObject;
-            switch (SwitchLab.Draw.Line)
+            switch (SwitchLab.Draw.Triangles)
             {
                 case SwitchLab.Draw.Line:
                     drawObject = DrawLines;
@@ -37,61 +37,80 @@ namespace _3DGraphics.Classes
                     drawObject = DrawLinesRGB;
                     break;
                 case SwitchLab.Draw.Triangles:
-                    drawObject = DrawLineTriangles;
+                    drawObject = DrawTriangles;
                     break;
             }
-            Parallel.ForEach(GeometricVertexIndexs, vertexIndex =>
-            {
-                var points = new Point[vertexIndex.Length];
-                for (var i = 0; i < vertexIndex.Length; i++)
-                {
-                    ref var coordinate = ref GeometricVertexСoordinates[vertexIndex[i]];
-                    if (coordinate.X > widthMaxReder || widthMinReder > coordinate.X || coordinate.Y > heightMaxReder || heightMinReder > coordinate.Y)
-                    {
-                        points = null;
-                        break;
-                    }
-                    points[i] = new Point(Convert.ToInt32(coordinate.X), Convert.ToInt32(coordinate.Y));
-                }
 
-                drawObject(points, vertexIndex, rgbBitmap, bitmapData, colorInt, widthZone, heightZone);
-            });
+            ///
+
+            const int SHIFT = 100;
+
+            var points = new PointF[]
+            {
+                new PointF(SHIFT,SHIFT),
+                new PointF(SHIFT*2,SHIFT),
+                new PointF(SHIFT*2,SHIFT * 2),
+                new PointF(SHIFT*3,SHIFT*2),
+            };
+
+
+            drawObject(points, new int[4], rgbBitmap, bitmapData, colorInt, widthZone, heightZone);
+
+            ///
+
+            /*            Parallel.ForEach(geometricVertexIndexs, vertexIndex =>
+                        {
+                            var points = new PointF[vertexIndex.Length];
+                            for (var i = 0; i < vertexIndex.Length; i++)
+                            {
+                                ref var coordinate = ref geometricVertexСoordinates[vertexIndex[i]];
+                                if (coordinate.X > widthMaxReder || widthMinReder > coordinate.X || coordinate.Y > heightMaxReder || heightMinReder > coordinate.Y)
+                                {
+                                    points = null;
+                                    break;
+                                }
+                                points[i] = new PointF(coordinate.X, coordinate.Y);
+                            }
+
+                            drawObject(points, vertexIndex, rgbBitmap, bitmapData, colorInt, widthZone, heightZone);
+                        });*/
+
             bitmap.UnlockBits(bitmapData);
         }
 
-        private static unsafe void DrawLine(int* rgbBitmap, int stride, int colorInt, Point point1, Point point2, int widthZone, int heightZone)
+        private static unsafe void DrawLine(int* rgbBitmap, int stride, int colorInt, PointF point1, PointF point2, int widthZone, int heightZone)
         {
-            int dx = point2.X - point1.X;
-            int dy = point2.Y - point1.Y;
-            int steps = Math.Max(Math.Abs(dx), Math.Abs(dy));
+            var dx = point2.X - point1.X;
+            var dy = point2.Y - point1.Y;
+            var steps = Math.Max(Math.Abs(dx), Math.Abs(dy));
 
-            float XIncrement = dx / (float)steps;
-            float YIncrement = dy / (float)steps;
+            var XIncrement = dx / (float)steps;
+            var YIncrement = dy / (float)steps;
 
-            float X = point1.X;
-            float Y = point1.Y;
+            float x = point1.X;
+            float y = point1.Y;
             int index;
 
             var strideInt = stride / 4;
 
             for (var i = 0; i <= steps; i++)
             {
-                if (X > widthZone || X < 0 || Y > heightZone || Y < 0)
+                if (x > widthZone || x < 0 || y > heightZone || y < 0)
                 {
-                    X += XIncrement;
-                    Y += YIncrement;
+                    x += XIncrement;
+                    y += YIncrement;
                     continue;
                 }
 
-                index = ((int)Math.Round(X)) + ((int)Math.Round(Y) * strideInt);
+                index = ((int)Math.Ceiling(x)) + ((int)Math.Ceiling(y) * strideInt);
                 rgbBitmap[index] = colorInt;
 
-                X += XIncrement;
-                Y += YIncrement;
+                x += XIncrement;
+                y += YIncrement;
             }
         }
 
-        public static unsafe void DrawLines(Point[] points, int[] vertexIndex, int* rgbBitmap, BitmapData bitmapData, int colorInt, int widthZone, int heightZone)
+        public static unsafe void DrawLines(PointF[] points, int[] vertexIndex, int* rgbBitmap, BitmapData bitmapData, int colorInt, int widthZone, int heightZone)
         {
             if (points != null)
             {
@@ -103,7 +122,7 @@ namespace _3DGraphics.Classes
             }
         }
 
-        public static unsafe void DrawLinesRGB(Point[] points, int[] vertexIndex, int* rgbBitmap, BitmapData bitmapData, int colorInt, int widthZone, int heightZone)
+        public static unsafe void DrawLinesRGB(PointF[] points, int[] vertexIndex, int* rgbBitmap, BitmapData bitmapData, int colorInt, int widthZone, int heightZone)
         {
             if (points != null)
             {
@@ -115,9 +134,9 @@ namespace _3DGraphics.Classes
 
                 for (var i = 0; i < vertexIndex.Length - 1; i++)
                 {
-                    r += points[i].X + rand.Next(100);
-                    g += points[i].Y + rand.Next(100);
-                    b += points[i].X + points[i].Y + rand.Next(100);
+                    r += Convert.ToInt32(points[i].X + rand.Next(100));
+                    g += Convert.ToInt32(points[i].Y + rand.Next(100));
+                    b += Convert.ToInt32(points[i].X + points[i].Y + rand.Next(100));
                 }
 
                 colorInt = Color.FromArgb(255, Math.Abs(r % 255), Math.Abs(g % 255), Math.Abs(b % 255)).ToArgb();
@@ -129,9 +148,47 @@ namespace _3DGraphics.Classes
             }
         }
 
-        public static unsafe void DrawLineTriangles(Point[] points, int[] vertexIndex, int* rgbBitmap, BitmapData bitmapData, int colorInt, int widthZone, int heightZone)
+        public static unsafe void DrawTriangles(PointF[] points, int[] vertexIndex, int* rgbBitmap, BitmapData bitmapData, int colorInt, int widthZone, int heightZone)
         {
+            if (points != null)
+            {
+                //Array.Sort(points, (p1, p2) => p1.X.CompareTo(p2.X));
+                Array.Sort(points, (p1, p2) => p2.Y.CompareTo(p1.Y));
 
+                int YIncrement = 1;
+
+                for (var indP = 0; indP < vertexIndex.Length - 2; indP++)
+                {
+                    float y = points[indP].Y;
+                    float x1 = points[indP].X;
+                    float x2 = points[indP].X;
+                    float minY = points[indP + 1].Y;
+
+                    var dx1 = points[indP + 1].X - points[indP].X;
+                    var dx2 = points[indP + 2].X - points[indP].X;
+
+                    var steps = points[indP].Y - points[indP + 2].Y;
+                    var XIncrement1 = dx1 / (float)steps;
+                    var XIncrement2 = dx2 / (float)steps;
+
+                    for (var i = 0; i <= steps; i++)
+                    {
+                        if (y <= minY)
+                        {
+                            x1 = points[indP + 1].X;
+                            dx1 = points[indP + 2].X - points[indP + 1].X;
+                            XIncrement1 = dx1 / (float)steps;
+                            minY = points[indP + 2].Y;
+                        }
+
+                        DrawLine(rgbBitmap, bitmapData.Stride, colorInt, new PointF(x1, y), new PointF(x2, y), widthZone, heightZone);
+
+                        x1 += XIncrement1;
+                        x2 += XIncrement2;
+                        y -= YIncrement;
+                    }
+                }
+            }
         }
     }
 }
